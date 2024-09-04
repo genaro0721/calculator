7afc5b50c62f1af0b97f59f1ca6cc1af438c177e
genaro <genaro.ramirez721@gmail.com>
2024-09-04 07:51:47 -0300

    requirement 1

---
+++ Calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Calculator Calculator = new Calculator();
+int result = Calculator.Add("-3,9");
+Console.WriteLine(result);
+
+public class Calculator
+{
+    public int Add(string numbers)
+    {
+        if (string.IsNullOrEmpty(numbers)) return 0;
+
+        var stringNumbers = numbers.Split(",");
+        if (stringNumbers.Length > 2) throw new ArgumentException("Maximum two numbers allowed");
+
+        int sum = 0;
+        foreach (var number in stringNumbers)
+        {
+            if (int.TryParse(number, out var parsedNumber))
+            {
+                sum += parsedNumber;
+            }
+        }
+        return sum;
+    }
+}
