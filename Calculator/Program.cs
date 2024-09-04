8313a9b8475b2a68cc6f92ce8e30ef5f0a45df99
genaro <genaro.ramirez721@gmail.com>
2024-09-04 07:53:30 -0300

    requirement 2

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,6 +1,6 @@
 ﻿
 Calculator Calculator = new Calculator();
-int result = Calculator.Add("-3,9");
+int result = Calculator.Add("1,2,3,4,5,6,7,8,9,10,11,12");
 Console.WriteLine(result);
 
 public class Calculator
@@ -10,7 +10,6 @@ public class Calculator
         if (string.IsNullOrEmpty(numbers)) return 0;
 
         var stringNumbers = numbers.Split(",");
-        if (stringNumbers.Length > 2) throw new ArgumentException("Maximum two numbers allowed");
 
         int sum = 0;
         foreach (var number in stringNumbers)
